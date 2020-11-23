7af3c0b728cb8ea6559a9cb0678b05a20b53fd02
PrathyushaLakkireddy <prathyusha@vitwit.com>
2020-11-23 16:11:30 +0530

    Updated script

--- targets/install_script.sh
+++ targets/install_script.sh
@@ -4,7 +4,7 @@ set -e
 
 cd $HOME
 
-echo "------ checking for go if it's not installed then it will be installing here -----"
+echo "------ checking for go, if it's not installed then it will be installed here -----"
 
 command_exists () {
     type "$1" &> /dev/null ;
