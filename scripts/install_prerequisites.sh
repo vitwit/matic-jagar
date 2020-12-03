0021f300f0c1f0dd3b11e28c5251d073a396bc08
kaustubhkapatral <54210167+kaustubhkapatral@users.noreply.github.com>
2020-12-03 16:22:49 +0530

    Update install_prerequisites.sh

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -22,7 +22,10 @@ else
   sudo mv go /usr/local
 
   echo "------ Update bashrc ---------------"
-
+  export GOPATH=$HOME/go
+  export GOROOT=/usr/local/go
+  export GOBIN=$GOPATH/bin
+  export PATH=$PATH:/usr/local/go/bin:$GOBIN
   echo "" >> ~/.bashrc
   echo 'export GOPATH=$HOME/go' >> ~/.bashrc
   echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
