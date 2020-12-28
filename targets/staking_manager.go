c4f4408eceb50e40b576830d1f011f0082f2f4f8
PrathyushaLakkireddy <prathyusha@vitwit.com>
2020-12-28 12:45:34 +0530

    Fixed commission_rate

--- targets/staking_manager.go
+++ targets/staking_manager.go
@@ -103,10 +103,8 @@ func GetCommissionRate(ops types.HTTPOptions, cfg *config.Config, c client.Clien
 				return
 			}
 
-			rate := utils.ConvertWeiToEth(commissionRate)
-
 			var fee float64
-			f, err := strconv.ParseFloat(rate, 64)
+			f, err := strconv.ParseFloat(commissionRate.String(), 64)
 			if err != nil {
 				fee = 0
 			} else {
@@ -203,6 +201,7 @@ func GetEncodedData(ops types.HTTPOptions, cfg *config.Config, c client.Client, 
 // EthCall will returns the validator share contract method response
 func EthCall(ops types.HTTPOptions, cfg *config.Config, c client.Client, dataHash string) (eth types.EthResult) {
 	contractAddress := GetValContractAddress(cfg, c)
+
 	data := types.Payload{
 		Jsonrpc: "2.0",
 		Method:  "eth_call",
